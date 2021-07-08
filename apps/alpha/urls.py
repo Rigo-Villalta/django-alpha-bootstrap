a94e6f3687cd4471a6d9284e5f1944efecf9653f
Rigo-Villalta <40904876+Rigo-Villalta@users.noreply.github.com>
2021-07-07 18:54:20 -0600

    Views for 403, 404 and 500 HTTP codes

--- apps/alpha/urls.py
+++ apps/alpha/urls.py
@@ -5,4 +5,7 @@ from django.urls import path
 app_name = "alpha"
 urlpatterns = [
     path("", TemplateView.as_view(template_name="alpha/home.html"), name="home"),
+    path("403", TemplateView.as_view(template_name="403.html"), name="403"),
+    path("404", TemplateView.as_view(template_name="404.html"), name="404"),
+    path("500", TemplateView.as_view(template_name="500.html"), name="500")
 ]
